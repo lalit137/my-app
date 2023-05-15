0d42d12c2119d8e3ffc4618ad0f95357db53fdbb
lalit137 <lalit.saud137@gmail.com>
2023-05-15 13:56:59 +0545

    filtered by year

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,19 @@ const DUMMY_EXPENSES  = [
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2019, 2, 28),
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'New year)',
+    amount: 450,
+    date: new Date(2022, 5, 12),
   },
 ];
 function App() {
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -10,6 +10,10 @@ function Expenses(props) {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     };
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
@@ -33,7 +37,7 @@ function Expenses(props) {
                 amount={props.items[3].amount}
                 date={props.items[3].date}
             /> */}
-            {props.items.map((expense) => (
+            {filteredExpenses.map((expense) => (
                 <ExpenseItem 
                 key={expense.id}
                 title={expense.title} 
