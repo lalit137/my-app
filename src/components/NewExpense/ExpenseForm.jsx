d708e376a0fcce57f6057853b515c269845f0004
lalit137 <lalit.saud137@gmail.com>
2023-05-16 13:25:14 +0545

    chart component added

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //   enteredTitle: '',
+    //   enteredAmount: '',
+    //   enteredDate: '',
+    // });
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
         // console.log(event.target.value);
-    }
+        // setUserInput({
+        //   ...userInput,
+        //   enteredTitle: event.target.value,
+        // });
+        // setUserInput((prevState) => {
+        //   return { ...prevState, enteredTitle: event.target.value };
+        // });
+    };
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
-    }
+        // setUserInput({
+        //   ...userInput,
+        //   enteredAmount: event.target.value,
+        // });
+    };
+
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //   ...userInput,
+        //   enteredDate: event.target.value,
+        // });
     }
 
     const submitHandler = (event) => {
@@ -22,7 +43,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
         props.onSaveExpenseData(expenseData);
@@ -33,23 +54,39 @@ const ExpenseForm = (props) => {
 
     return (
         <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
                 </div>
-                <div className="new-expense__control">
+                <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input
+                        type='number'
+                        min='0.01'
+                        step='0.01'
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}
+                    />
                 </div>
-                <div className="new-expense__control">
+                <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" value={enteredDate} min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                    <input
+                        type='date'
+                        min='2019-01-01'
+                        max='2022-12-31'
+                        value={enteredDate}
+                        onChange={dateChangeHandler}
+                    />
                 </div>
-
-                <div className="new-expense__actions">
-                    <button type="submit">Add Expense</button>
-                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button type='submit'>Add Expense</button>
             </div>
         </form>
     );
